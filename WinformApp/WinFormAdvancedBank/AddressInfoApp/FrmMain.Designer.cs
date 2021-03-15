caf2bcb7dcde1d727c0866fed5a276c6094d6da7
최대현 <eogusalrns@naver.com>
2021-03-15 17:27:09 +0900

    03_15 8교시

---
+++ WinformApp/WinFormAdvancedBank/AddressInfoApp/FrmMain.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace AddressInfoApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvAddress = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtIdx = new System.Windows.Forms.TextBox();
+            this.TxtAddr = new System.Windows.Forms.TextBox();
+            this.TxtFullName = new System.Windows.Forms.TextBox();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnInsert = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.MtbMobile = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAddress)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DgvAddress
+            // 
+            this.DgvAddress.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvAddress.Location = new System.Drawing.Point(12, 12);
+            this.DgvAddress.Name = "DgvAddress";
+            this.DgvAddress.RowTemplate.Height = 23;
+            this.DgvAddress.Size = new System.Drawing.Size(377, 271);
+            this.DgvAddress.TabIndex = 0;
+            this.DgvAddress.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvAddress_CellClick);
+            this.DgvAddress.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvAddress_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.MtbMobile);
+            this.groupBox1.Controls.Add(this.BtnDelete);
+            this.groupBox1.Controls.Add(this.BtnUpdate);
+            this.groupBox1.Controls.Add(this.BtnInsert);
+            this.groupBox1.Controls.Add(this.BtnClear);
+            this.groupBox1.Controls.Add(this.TxtFullName);
+            this.groupBox1.Controls.Add(this.TxtAddr);
+            this.groupBox1.Controls.Add(this.TxtIdx);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 289);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 116);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "입력창";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "주소";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "이름";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "전화번호";
+            // 
+            // TxtIdx
+            // 
+            this.TxtIdx.Location = new System.Drawing.Point(31, 14);
+            this.TxtIdx.Name = "TxtIdx";
+            this.TxtIdx.ReadOnly = true;
+            this.TxtIdx.Size = new System.Drawing.Size(28, 21);
+            this.TxtIdx.TabIndex = 1;
+            this.TxtIdx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtIdx_KeyPress);
+            // 
+            // TxtAddr
+            // 
+            this.TxtAddr.Location = new System.Drawing.Point(41, 48);
+            this.TxtAddr.MaxLength = 200;
+            this.TxtAddr.Name = "TxtAddr";
+            this.TxtAddr.Size = new System.Drawing.Size(121, 21);
+            this.TxtAddr.TabIndex = 3;
+            this.TxtAddr.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtAddr_KeyPress);
+            // 
+            // TxtFullName
+            // 
+            this.TxtFullName.Location = new System.Drawing.Point(113, 14);
+            this.TxtFullName.MaxLength = 12;
+            this.TxtFullName.Name = "TxtFullName";
+            this.TxtFullName.Size = new System.Drawing.Size(109, 21);
+            this.TxtFullName.TabIndex = 2;
+            this.TxtFullName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtFullName_KeyPress);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(6, 81);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 5;
+            this.BtnClear.Text = "초기화";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnInsert
+            // 
+            this.BtnInsert.Location = new System.Drawing.Point(87, 81);
+            this.BtnInsert.Name = "BtnInsert";
+            this.BtnInsert.Size = new System.Drawing.Size(75, 23);
+            this.BtnInsert.TabIndex = 6;
+            this.BtnInsert.Text = "추가";
+            this.BtnInsert.UseVisualStyleBackColor = true;
+            this.BtnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(168, 81);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.BtnUpdate.TabIndex = 7;
+            this.BtnUpdate.Text = "수정";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(249, 81);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 8;
+            this.BtnDelete.Text = "삭제";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // MtbMobile
+            // 
+            this.MtbMobile.Location = new System.Drawing.Point(232, 48);
+            this.MtbMobile.Mask = "000-9000-0000";
+            this.MtbMobile.Name = "MtbMobile";
+            this.MtbMobile.Size = new System.Drawing.Size(92, 21);
+            this.MtbMobile.TabIndex = 4;
+            this.MtbMobile.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MtbMobile_KeyPress);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 414);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.DgvAddress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "주소록 앱";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAddress)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvAddress;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox MtbMobile;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnInsert;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.TextBox TxtFullName;
+        private System.Windows.Forms.TextBox TxtAddr;
+        private System.Windows.Forms.TextBox TxtIdx;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinformApp/WinFormAdvancedBank/AddressInfoApp/FrmMain.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressInfoApp
+{
+    public partial class FrmMain : Form
+    {
+        string connString = "Data Source=127.0.0.1;Initial Catalog = PMS; Persist Security Info=True;" +
+            "User ID = sa;Password=mssql_p@ssw0rd!";
+        public FrmMain()
+        {
+            InitializeComponent();
+            TxtIdx.Focus();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+            RefreshData();
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            int.TryParse(TxtIdx.Text, out int result);
+            if (result > 0)
+            {
+                MessageBox.Show("초기화를 진행하십시오");
+                return;
+            }
+            if (string.IsNullOrEmpty(TxtFullName.Text) || string.IsNullOrEmpty(MtbMobile.Text))
+            {
+                MessageBox.Show("값을 입력하세요."); 
+                return;
+            }
+                
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = $"INSERT INTO dbo.Address" +
+                    $" (FullName, " +
+                    $" Mobile, " +
+                    $" Address, " +
+                    $" RegId, " +
+                    $" RegDate) " +
+                    $" VALUES " +
+                    $" ('{TxtFullName.Text}'," +
+                    $"  '{MtbMobile.Text.Replace("-","")}'," +
+                    $"  '{TxtAddr.Text}'," +
+                    $"  'admin'," +
+                    $"  GETDATE() ); ";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if(cmd.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("입력 성공");
+                }
+                else
+                {
+                    MessageBox.Show("입력 실패");
+                }
+            }
+            RefreshData();
+            ClearInput();
+        }
+
+        private void RefreshData()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                //SSMS 테이블 스크립팅 메뉴활용
+                string query = "SELECT Idx " +
+                               "     , FullName " +
+                               "     , Mobile " +
+                               "     , Address " +
+                               "  FROM dbo.Address ";
+                //SqlCommand, SqlDataReader or Object 사용방법 1
+                //SqlDataAdapter, DataSet
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+                DgvAddress.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            int.TryParse(TxtIdx.Text, out int result);
+            if (result == 0)
+            {
+                MessageBox.Show("데이터를 선택하세요");
+                return;
+            }
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = $"UPDATE Address " +
+                    $" SET " +
+                    $" FullName = '{TxtFullName.Text}'," +
+                    $" Mobile = '{MtbMobile.Text.Replace("-","")}' ," +
+                    $" Address = '{TxtAddr.Text}' ," +
+                    $" ModId = 'admin'," +
+                    $" ModDate = GETDATE()" +
+                    $" WHERE Idx = {result} ";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (cmd.ExecuteNonQuery() == 1)
+                    MessageBox.Show("수정 성공");
+                else
+                    MessageBox.Show("수정 실패");
+            }
+            ClearInput();
+            RefreshData();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            int.TryParse(TxtIdx.Text, out int result);
+            if (result == 0)
+            {
+                MessageBox.Show("데이터를 선택하세요");
+                return;
+            }
+            using(SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = $"DELETE FROM Address WHERE idx = {result}";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (cmd.ExecuteNonQuery() == 1)
+                    MessageBox.Show("삭제 성공");
+                else
+                    MessageBox.Show("삭제 실패");
+            }
+            ClearInput();
+            RefreshData();
+        }
+
+        private void MtbMobile_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void TxtFullName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ( e.KeyChar == 13)
+            {
+                TxtAddr.Focus();
+            }
+        }
+
+        private void TxtAddr_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                MtbMobile.Focus();
+            }
+        }
+
+        private void TxtIdx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                TxtFullName.Focus();
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            RefreshData();
+            ClearInput();
+        }
+
+        private void ClearInput()
+        {
+            TxtIdx.Text = TxtFullName.Text = TxtAddr.Text = MtbMobile.Text = "";
+        }
+
+
+        private void DgvAddress_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var selectData = DgvAddress.Rows[e.RowIndex].Cells;
+            TxtIdx.Text = selectData[0].Value.ToString();
+            TxtFullName.Text = selectData[1].Value.ToString();
+            MtbMobile.Text = selectData[2].Value.ToString();
+            TxtAddr.Text = selectData[3].Value.ToString();
+        }
+
+        private void DgvAddress_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var selectData = DgvAddress.Rows[e.RowIndex].Cells;
+            TxtIdx.Text = selectData[0].Value.ToString();
+            TxtFullName.Text = selectData[1].Value.ToString();
+            MtbMobile.Text = selectData[2].Value.ToString();
+            TxtAddr.Text = selectData[3].Value.ToString();
+        }
+    }
+}
